eaf35e9259d1f580d88598c4e90fc75273b59e0f
reggiecril <reggiecril0618@gmail.com>
2019-11-29 15:56:00 +0800

    init commit

---
+++ src/main/java/com/cloudaping/cloudaping/CloudapingApplication.java
@@ -0,0 +1,13 @@
+package com.cloudaping.cloudaping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudapingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudapingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudaping/cloudaping/CloudapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudaping.cloudaping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudapingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
